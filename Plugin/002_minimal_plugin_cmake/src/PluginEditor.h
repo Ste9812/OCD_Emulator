dbcdd118268d9e94f3664c342a8e8b5c8442b5f7
Ste9812 <stefano.ravasi@mail.polimi.it>
2024-08-06 12:26:47 +0200

    up

---
+++ Plugin/002_minimal_plugin_cmake/src/PluginEditor.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+class OCD_EmuAudioProcessorEditor  : public juce::AudioProcessorEditor,
+                                        public Slider::Listener, 
+                                        public Button::Listener
+{
+public:
+    OCD_EmuAudioProcessorEditor (OCD_EmuAudioProcessor&);
+    ~OCD_EmuAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+    //==============================================================================
+    void sliderValueChanged(Slider *slider) override;
+    void buttonClicked(Button* btn) override;
+
+private:
+    juce::Slider driveRot;
+    juce::TextButton switchBtn{"LP"};
+    juce::Slider toneRot;
+    juce::Slider volumeRot;
+    juce::TextButton bypassBtn{"ON"};
+
+    juce::Label driveLbl{{}, "Drive"};
+    juce::Label toneLbl{{}, "Tone"};
+    juce::Label volumeLbl{{}, "Volume"};
+
+    OCD_EmuAudioProcessor& audioProcessor;
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (OCD_EmuAudioProcessorEditor)
+};
